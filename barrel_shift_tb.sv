6fe0ae79573f851692bc14ae11436b460fb73e27
jmqian1009 <1830010557@qq.com>
2024-11-09 15:27:10 +0800

    use generate

--- barrel_shift_tb.sv
+++ barrel_shift_tb.sv
@@ -1,5 +1,5 @@
 module barrel_shift_tb;
-parameter N = 8;
+parameter N = 32;
 //
 logic [N-1:0]         data_in;
 logic [N-1:0]         data_out;
@@ -10,14 +10,28 @@ logic [N-1:0]         data_out_ref;
 logic                 error;
 logic [N-1:0]         mask0;
 logic [N-1:0]         mask1;
+logic                 clk;
+//clk
+initial begin
+    clk = 1'b0;
+    forever begin
+        #5 clk = ~clk;
+    end
+end
+//
+always_ff@(posedge clk) begin
+    if(error) begin
+        $display("test failed");
+    end
+end
 //mask0,1
 assign mask0 = (1<<shift_num) - 1;
-assign mask1 = (1<<(8-shift_num))-1;
+assign mask1 = (1<<(N-shift_num))-1;
 //drv
 initial begin
     forever begin
-        data_in = {$random()} % 256;
-        shift_num = {$random()} % 8;
+        data_in = {$random()} % ((N+1)'(1'd1)<<N);
+        shift_num = {$random()} % N;
         op = {$random()} % 5;         //0,1,2,3,4
         #10;
     end
@@ -30,8 +44,8 @@ always_comb begin
         3'd0: data_out_ref = data_in >> shift_num; 
         3'd1: data_out_ref = $signed(data_in) >>> shift_num;
         3'd2: data_out_ref = data_in << shift_num;
-        3'd3: data_out_ref = ((data_in & mask0) << (8-shift_num)) + (data_in >> shift_num);                          //rotate shift right
-        3'd4: data_out_ref = (data_in << shift_num) + ((data_in & ~mask1) >> (8-shift_num));                         //rotate shift left
+        3'd3: data_out_ref = ((data_in & mask0) << (N-shift_num)) + (data_in >> shift_num);                          //rotate shift right
+        3'd4: data_out_ref = (data_in << shift_num) + ((data_in & ~mask1) >> (N-shift_num));                         //rotate shift left
         default: data_out_ref = data_in;
     endcase
 end
@@ -43,12 +57,13 @@ initial begin
 end
 
 initial begin
-    #10000
+    #100000
+    $display("test pass");
     $finish;
 end
 
 barrel_shift #
-( .N(8) ) 
+( .N(N) ) 
 U (
   .data_in  (data_in  ),
   .shift_num(shift_num),
